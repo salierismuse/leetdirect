f1b1c4275c5d5945ee8ce4c630e400467fd33cf8
Isaac <Isaacformato@gmail.com>
2025-05-19 22:20:11 -0400

    cleanup

--- options.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("save").addEventListener("click", () => {
-	const input = document.getElementById("domains").value;
-	const domains = input.split("\n").map(d => d.trim()).filter(Boolean);
-	chrome.storage.sync.set({ bannedDomains: domains });
-});
